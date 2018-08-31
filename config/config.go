1e2d3eb5ae714c85fdb1cdffe5fe62b0f94416dc
gongyao <825612127@qq.com>
2018-08-31 11:10:44 +0800

    [提交代码]-gopath

---
+++ config/config.go
@@ -0,0 +1,17 @@
+package config
+
+const env = "local"
+
+func Get(item string) string {
+	type confMap map[string]string
+
+	conf := make(map[string]confMap)
+
+	conf["local"] = map[string]string{
+		"Port": "9002",
+		"Mysql": "root:S12p_w99Q@tcp(139.198.5.192:3306)/xiangxin0828?charset=utf8",
+		"Redis": "119.254.108.200:6379",
+	}
+
+	return  conf[env][item]
+}
