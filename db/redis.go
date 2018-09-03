7f64240ea17cc504e76dc36d376b6d31b3994491
gongyao <825612127@qq.com>
2018-09-03 10:33:17 +0800

    [提交代码]

--- db/redis.go
+++ db/redis.go
@@ -1,1 +1,43 @@
 package db
+
+import (
+	"mygo/config"
+	"time"
+
+	"github.com/garyburd/redigo/redis"
+)
+
+var pool *redis.Pool
+
+// GetRedis 获取redis实例
+func GetRedis() redis.Conn {
+	return pool.Get()
+}
+
+func newPool(server, password string) *redis.Pool {
+	return &redis.Pool{
+		MaxIdle:     3,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", server)
+			if err != nil {
+				return nil, err
+			}
+			return c, err
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			if time.Since(t) < time.Minute {
+				return nil
+			}
+			_, err := c.Do("PING")
+			return err
+		},
+	}
+}
+
+func init() {
+	server := config.Get("Redis")
+	password := ""
+
+	pool = newPool(server, password)
+}
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/garyburd/redigo/redis"
 	"mygo/db"
 	"mygo/model"
 )
@@ -9,8 +10,24 @@ import (
 func main() {
 
 	Db := db.Get()
+	defer Db.Close()
 
 	push := model.GetPush(Db,32001)
 
 	fmt.Printf("%v", push)
+
+	Re := db.GetRedis()
+	defer Re.Close()
+	_, err := Re.Do("SET", "mykey", "superWang")
+	if err != nil {
+
+	}
+
+	value, err := redis.String(Re.Do("GET", "mykey"))
+	if err != nil {
+		fmt.Println("redis get failed:", err)
+	} else {
+		fmt.Printf("Get mykey: %v \n", value)
+	}
+	//fmt.Printf("value %v", value)
 }
